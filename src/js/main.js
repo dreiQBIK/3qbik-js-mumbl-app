9f57eef9fb1d098bc7fc84bac82236f090f59dcd
Joscha Holzhäuer <j.holzhaeuer@outlook.com>
2017-02-11 19:49:41 +0100

    FEATURE: love functionality added

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,95 @@ JAVASCRIPT: MAIN
 
 ******************************************************************/
 
+const names = [
+    {
+        "name": "Jimmy",
+        "sex": "male",
+        "shown": false,
+        "loved": false
+    },
+    {
+        "name": "Johnny",
+        "sex": "male",
+        "shown": false,
+        "loved": false
+    },
+    {
+        "name": "Kurt",
+        "sex": "male",
+        "shown": false,
+        "loved": false
+    },
+    {
+        "name": "Peter",
+        "sex": "male",
+        "shown": false,
+        "loved": false
+    },
+    {
+        "name": "Lisa",
+        "sex": "female",
+        "shown": false,
+        "loved": false
+    },
+    {
+        "name": "Anna",
+        "sex": "female",
+        "shown": false,
+        "loved": false
+    },
+    {
+        "name": "Gerda",
+        "sex": "female",
+        "shown": false,
+        "loved": false
+    },
+    {
+        "name": "Frauke",
+        "sex": "female",
+        "shown": false,
+        "loved": false
+    }
+];
+
+const colors = [
+    {
+        "main": "#66B2C3",
+        "second": "#6AC9DE",
+        "third": "#75DAF0"
+    },
+    {
+        "main": "#CA6C9D",
+        "second": "#E06DA9",
+        "third": "#F571B6"
+    },
+    {
+        "main": "#DAD06F",
+        "second": "#ECE068",
+        "third": "#F3E76B"
+    },
+    {
+        "main": "#DE9459",
+        "second": "#F39B54",
+        "third": "#FCA661"
+    },
+    {
+        "main": "#B27CD1",
+        "second": "#BE7BE5",
+        "third": "#C77FF1"
+    },
+    {
+        "main": "#6BC48A",
+        "second": "#65D28B",
+        "third": "#76F1A0"
+    },
+    {
+        "main": "#8DCE73",
+        "second": "#8DDC6E",
+        "third": "#97EA76"
+    }
+];
+
 // const jsonFile = 'names.json';
 // const names = [];
 
@@ -12,87 +101,6 @@ JAVASCRIPT: MAIN
 
 // console.log(names);
 
-const names = [
-    {
-        "name": "Jimmy",
-        "sex": "male",
-        "shown": false
-    },
-    {
-        "name": "Johnny",
-        "sex": "male",
-        "shown": false
-    },
-    {
-        "name": "Kurt",
-        "sex": "male",
-        "shown": false
-    },
-    {
-        "name": "Peter",
-        "sex": "male",
-        "shown": false
-    },
-    {
-        "name": "Lisa",
-        "sex": "female",
-        "shown": false
-    },
-    {
-        "name": "Anna",
-        "sex": "female",
-        "shown": false
-    },
-    {
-        "name": "Gerda",
-        "sex": "female",
-        "shown": false
-    },
-    {
-        "name": "Frauke",
-        "sex": "female",
-        "shown": false
-    }
-];
-
-const colors = [
-    {
-        "main": "#273438",
-        "second": "#6AC9DE",
-        "third": "#75DAF0"
-    },
-    {
-        "main": "#2E393C",
-        "second": "#E06DA9",
-        "third": "#F571B6"
-    },
-    {
-        "main": "#2E393C",
-        "second": "#ECE068",
-        "third": "#F3E76B"
-    },
-    {
-        "main": "#2E393C",
-        "second": "#F39B54",
-        "third": "#FCA661"
-    },
-    {
-        "main": "#2E393C",
-        "second": "#BE7BE5",
-        "third": "#C77FF1"
-    },
-    {
-        "main": "#2E393C",
-        "second": "#65D28B",
-        "third": "#76F1A0"
-    },
-    {
-        "main": "#2E393C",
-        "second": "#8DDC6E",
-        "third": "#97EA76"
-    }
-];
-
 
 /***********************************************************************
     ON LOAD
@@ -101,9 +109,22 @@ const colors = [
 // mumbl name on load
 mumblName();
 
-// listen on click on btn
-let btn = document.querySelector('.btn');
+// listen on click on mumbl btn
+let btn = document.querySelector('.btn--mumbl');
 btn.addEventListener('click', mumblName);
+btn.addEventListener('click', animateButton);
+
+// get currentName
+let currentName = names[names.findIndex(el => el.shown)];
+
+// listen on click on love btn
+let btnLove = document.querySelector('.btn--love');
+btnLove.addEventListener('click', loveName);
+btnLove.addEventListener('click', animateButton);
+
+// listen on click on delete btn
+// let btnDelete = document.querySelectorAll('.btn--delete');
+// btnDelete.forEach(el => el.addEventListener('click', deleteName));
 
 
 
@@ -115,12 +136,71 @@ function resetStatus() {
 
     // reset status
     names.forEach(function(el) {
+        if (el.loved) return;
         el.shown = false;
     });
+
+    // check if all names have already been loved
+    const allLoved = names.every(function(el) {
+        if (el.loved === true) {
+            return true;
+        }
+    });
+
+    // finish mumbl and change btn color
+    if (allLoved === true) {
+        return;
+    }
 
     // mumbl a random first name again
     mumblName();
 };
+
+function changeColors() {
+
+    // CSS changes
+    const body = document.querySelector('body');
+    const card = document.querySelector('.card');
+    const cardLogoWrapper = document.querySelector('.card__logo-wrapper');
+    const cardIcon = document.querySelector('.card__icon');
+    const listItem = document.querySelectorAll('.list__item');
+    // const listIcon = document.querySelector('.list__icon');
+    btn = document.querySelector('.btn--mumbl');
+    btnLove = document.querySelector('.btn--love');
+
+    // random color
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]; // random color as an object
+
+    // CSS color
+    body.style.backgroundColor = randomColor.main;
+    card.style.backgroundColor = randomColor.second;
+    cardLogoWrapper.style.backgroundColor = randomColor.second;
+    cardIcon.style.backgroundColor = randomColor.third;
+    listItem.forEach(el => el.style.backgroundColor = randomColor.second);
+    // listIcon.style.backgroundColor = randomColor.third;
+    btn.style.backgroundColor = randomColor.second;
+    btnLove.style.backgroundColor = randomColor.second;
+
+    // add animations
+    card.classList.add('flip');
+
+    // remove animations
+    setTimeout(function(){
+        card.classList.remove('flip');
+    }, 300);
+}
+
+function animateButton(e) {
+
+    // add animations
+    const clickedBtn = e.currentTarget;
+    clickedBtn.classList.add('press');
+
+    // remove animations
+    setTimeout(function(){
+        clickedBtn.classList.remove('press');
+    }, 300);
+}
 
 function mumblName() {
 
@@ -129,7 +209,7 @@ function mumblName() {
 
     // check if name was already shown
     // to prevent that the same name is shown multiple times
-    if (randomName.shown === false) {
+    if (randomName.shown === false && randomName.loved === false) {
 
         // change HTML of name
         const cardName = document.querySelector('.card__heading');
@@ -139,32 +219,14 @@ function mumblName() {
         const cardIcon = document.querySelector('.card__icon');
         cardIcon.innerHTML = (randomName.sex === 'male') ? '&#9794;' : '&#9792;';
 
-        // CSS changes
-        const body = document.querySelector('body');
-        const card = document.querySelector('.card');
-        const cardLogoWrapper = document.querySelector('.card__logo-wrapper');
-        btn = document.querySelector('.btn');
-
-        // random color
-        const randomColor = colors[Math.floor(Math.random() * colors.length)]; // random color as an object
-
-        // add flip animation
-        card.classList.add('flip');
-
-        // CSS color
-        body.style.backgroundColor = randomColor.main;
-        card.style.backgroundColor = randomColor.second;
-        cardLogoWrapper.style.backgroundColor = randomColor.second;
-        cardIcon.style.backgroundColor = randomColor.third;
-        btn.style.backgroundColor = randomColor.third;
+        // change colors and add animation
+        changeColors();
 
         // remember that this name was already shown
         randomName.shown = true;
 
-        // remove flip animation
-        setTimeout(function(){
-            card.classList.remove('flip');
-        }, 300);
+        // save current name
+        currentName = randomName;
 
     } else {
 
@@ -183,3 +245,51 @@ function mumblName() {
         }
     }
 }
+
+function loveName() {
+
+    // check if name is not already marked as loved
+    if (currentName.loved) return;
+
+    // mark name as loved
+    currentName.loved = true;
+
+    // save name to loved list
+    const list = document.querySelector('.list');
+    const newListItem = document.createElement('li');
+    // const newDeleteButton = document.createElement('a');
+    // const newDeleteIcon = document.createElement('span');
+    // const newDeleteIconText = document.createTextNode('x');
+    const newListItemText = document.createTextNode(`${currentName.name}`);
+
+    newListItem.className = 'list__item';
+    newListItem.setAttribute('data-name', `${currentName.name}`);
+    newListItem.appendChild(newListItemText);
+
+    // newDeleteButton.className = 'btn btn--delete';
+    // newDeleteButton.setAttribute('href', '#');
+    // newDeleteIcon.className = 'btn__icon btn__icon--x';
+    // newDeleteIcon.appendChild(newDeleteIconText);
+
+    // newListItem.appendChild(newDeleteButton);
+    // newDeleteButton.appendChild(newDeleteIcon);
+
+    list.appendChild(newListItem);
+
+    // mumbl next name
+    mumblName();
+}
+
+// function deleteName(e) {
+
+//     // find out which name was clicked
+//     const deletedElement = e.currentTarget.parentElement.getAttribute('data-name');
+//     const deletedName = names.filter(el => el.name === deletedElement);
+
+//     // change loved status back to false
+//     deletedName.loved = false;
+
+//     // delete name from favorites list
+//     const list = document.querySelector('.list');
+//     list.removeChild(deletedElement);
+// }
